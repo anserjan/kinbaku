571ef74083107baa23c0d851572c096a78707678
s51051 <s51051@bht-berlin.de>
2023-02-10 20:38:03 +0100

    fix logic error

--- db/migrate/20220911174900_change_fields_from_spree_products.rb
+++ db/migrate/20220911174900_change_fields_from_spree_products.rb
@@ -2,7 +2,7 @@ class ChangeFieldsFromSpreeProducts < ActiveRecord::Migration[5.2]
     def change
         remove_column :spree_products, :date, :datetime
         remove_column :spree_products, :time, :datetime
-        change_column :spree_products, :date_and_time, :datetime, :precision => 6
+        add_column :spree_products, :date_and_time, :datetime, :precision => 6
     end
 end
   
