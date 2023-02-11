43c225a4e4b95b9b480e4b881c365f4acca43221
s51051 <s51051@bht-berlin.de>
2023-02-11 02:41:50 +0100

    use same sku on all creation methods, collect double code

--- app/overrides/dojo/spree/admin/products_controller/add_base_extensions.rb
+++ app/overrides/dojo/spree/admin/products_controller/add_base_extensions.rb
@@ -5,7 +5,19 @@ module Dojo
         module AddBaseExtensions
 
           def self.prepended(base)
-            
+            base.after_action :set_default_values, only: [:create]
+          end
+
+          private
+
+          def set_default_values
+            @product.stock_items.all.each do |si|
+              si.adjust_count_on_hand Dojo::Application.config.default_count_on_hand
+              si.update_attribute(:backorderable, false)
+            end
+            params[:product][:taxon_ids].empty? ? taxon_ids = "" : taxon_ids = params[:product][:taxon_ids].split(",").join("-")
+            @product.sku = "k-#{taxon_ids}-#{params[:product][:date_and_time].to_datetime.strftime("%Y%m%d%H%M")}"
+            @product.save
           end
 
           ::Spree::Admin::ProductsController.prepend self
--- app/overrides/dojo/spree/product/add_create_bulk.rb
+++ app/overrides/dojo/spree/product/add_create_bulk.rb
@@ -6,26 +6,21 @@ module Dojo
         def create_bulk params
           count_date = params[:product][:date_and_time].to_datetime
           for i in 1..params[:repeat_amount].to_i do
-            product = ::Spree::Product.create(
+            @product = ::Spree::Product.create(
               name: params[:product][:name] + count_date.strftime(" %d.%m.%Y"),
               description: params[:product][:description],
-              sku: set_sku(params[:product][:taxon_ids], count_date),
               price: params[:product][:price],
               date_and_time: count_date,
               taxon_ids: params[:product][:taxon_ids],
               available_on: params[:product][:available_on],
               shipping_category_id: params[:product][:shipping_category_id]
             )
-            product.stock_items.all.each do |si|
-              si.adjust_count_on_hand Dojo::Application.config.default_count_on_hand
-              si.update_attribute(:backorderable, false)
-            end
             if params[:repeat_pattern].eql? "weekly"
               count_date = params[:product][:date_and_time].to_datetime + i.weeks
             else
               count_date = params[:product][:date_and_time].to_datetime + i.months
             end
-            product.save
+            @product.save
           end
         end
 
@@ -40,11 +35,6 @@ module Dojo
           return_taxon_ids = []
           taxonomies.each { |k,v| return_taxon_ids << v }
           return return_taxon_ids
-        end
-        
-        def set_sku taxon_ids, date
-          taxon_ids &&= taxon_ids.split(",").join("-")
-          return "k-#{taxon_ids}#{date.strftime("-%Y%m%d")}"
         end
 
         ::Spree::Product.extend self
