de305c47985f901aac6c98bcd248335639cbf19b
= <aj@anserjan.de>
2023-03-18 19:51:05 +0100

    fix pages menu bar not saving

--- app/overrides/dojo/spree/admin/pages_controller/generate_slug.rb
+++ app/overrides/dojo/spree/admin/pages_controller/generate_slug.rb
@@ -9,6 +9,7 @@ module Dojo
               title: params[:page][:title],
               slug: "/" + params[:page][:title].downcase.split(" ").join("-"),
               body: params[:page][:body],
+              menu_bar_id: params[:page][:menu_bar_id].to_i,
               store_ids: ::Spree::Store.first.id
             )
             redirect_to admin_pages_path
