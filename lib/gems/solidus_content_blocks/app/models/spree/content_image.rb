8bff1e763a59d44856413065459e2cdf332bebd4
= <aj@anserjan.de>
2023-03-19 11:37:41 +0100

    tweak gallery styling decrease image size

--- lib/gems/solidus_content_blocks/app/models/spree/content_image.rb
+++ lib/gems/solidus_content_blocks/app/models/spree/content_image.rb
@@ -2,7 +2,7 @@ class Spree::ContentImage < Spree::Base
 
   has_one_attached :image do |attachable|
     attachable.variant :mini, resize_to_limit: [48, 48]
-    attachable.variant :gallery, resize_to_limit: [256,256]
+    attachable.variant :gallery, resize_to_limit: [300, 300]
     attachable.variant :small, resize_to_limit: [400, 400]
     attachable.variant :product, resize_to_limit: [680, 680]
     attachable.variant :large, resize_to_limit: [1200, 1200]
