4201ae57d3afa9db9a2bbd1666ff6c135be9c48a
= <aj@anserjan.de>
2023-08-21 15:51:33 +0200

    add three card teaser content block

--- lib/gems/solidus_content_blocks/lib/solidus_content_blocks/engine.rb
+++ lib/gems/solidus_content_blocks/lib/solidus_content_blocks/engine.rb
@@ -5,7 +5,7 @@ module SolidusContentBlocks
     config.autoload_paths += %W(#{config.root}/lib)
 
     config.content_block_types = [
-      "richtext", "images", "richtext_image_left", "richtext_image_right", "embed", "stage", "three_card_teaser", "image_coloumn", "center_image",
+      "richtext", "images", "richtext_image_left", "richtext_image_right", "embed", "stage", "three_card_teaser", "center_image",
     ]
 
     config.partition_enums = [
