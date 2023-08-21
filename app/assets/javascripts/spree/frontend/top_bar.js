e20a6d65732b40814758731816f2f0d3bbf67a43
= <aj@anserjan.de>
2023-08-21 17:21:56 +0200

    add page_gallery_Column to BE, fix menu toggle

--- app/assets/javascripts/spree/frontend/top_bar.js
+++ app/assets/javascripts/spree/frontend/top_bar.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const sub_menu_toggles = document.querySelectorAll(".sub__menu__toggle");
   sub_menu_toggles.forEach(sub_menu_toggle => {
     sub_menu_toggle.addEventListener("click", () => {
-      sub_menu_toggles.forEach(smt => {smt.classList.remove("active");});
-      sub_menu_toggle.classList.add("active");
+      // first close every menu before opening the specific one
+      sub_menu_toggles.forEach(to_close_sub_menu_toggle => {
+        if(to_close_sub_menu_toggle == sub_menu_toggle) { return; }
+        to_close_sub_menu_toggle.classList.remove("active");
+      })
+      sub_menu_toggle.classList.toggle("active");
     })
   });
 });
