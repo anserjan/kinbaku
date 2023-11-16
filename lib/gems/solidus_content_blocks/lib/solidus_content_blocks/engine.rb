fcb08aeed553394a805976162244f8304a285b12
= <aj@anserjan.de>
2023-11-16 13:50:39 +0100

    transform three card teaser to three squares

--- lib/gems/solidus_content_blocks/lib/solidus_content_blocks/engine.rb
+++ lib/gems/solidus_content_blocks/lib/solidus_content_blocks/engine.rb
@@ -5,7 +5,7 @@ module SolidusContentBlocks
     config.autoload_paths += %W(#{config.root}/lib)
 
     config.content_block_types = [
-      "richtext", "richtext_image_left", "richtext_image_right", "embed", "stage", "three_card_teaser", "center_image", "page_gallery_column"
+      "richtext", "richtext_image_left", "richtext_image_right", "embed", "stage", "three_squares", "center_image", "page_gallery_column"
     ]
 
     config.partition_enums = [
