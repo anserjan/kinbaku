de4dfcb13ecbfc3d33666e4e603441d9bdfb3b96
= <aj@anserjan.de>
2024-02-09 15:24:58 +0100

    improve create bulk performance

--- app/overrides/dojo/spree/admin/products_controller/change_create.rb
+++ app/overrides/dojo/spree/admin/products_controller/change_create.rb
@@ -5,8 +5,9 @@ module Dojo
         module ChangeCreate
 
           def create
+            @image = File.open(Rails.root.join("app", "assets", "images", "placeholder_products.jpeg"))
             if params[:repeat_pattern].eql? "single"
-              product = ::Spree::Product.create( 
+              @product = ::Spree::Product.create( 
                 name: set_default_name(params, params[:product][:date_and_time]),
                 sku: set_sku(params, params[:product][:date_and_time]),
                 description: params[:product][:description],
@@ -17,9 +18,9 @@ module Dojo
                 shipping_category_id: params[:product][:shipping_category_id], 
                 taxon_ids: params[:product][:taxon_ids]
               )
-              set_stock_items(product)
-              set_default_image(product)
-              product.save
+              set_stock_items
+              set_default_image
+              @product.save
             else
               create_bulk(params)
             end
@@ -28,23 +29,13 @@ module Dojo
 
           private
           
-          def set_default_name params, new_date
+          def set_default_name params, date
+            new_date = date.to_datetime.strftime(" %d.%m.%Y %H:%M")
             if params[:product][:name].present?
-              return params[:product][:name] + new_date.to_datetime.strftime(" %d.%m.%Y %H:%M")
+              return params[:product][:name] + new_date
             else
-              return "Shibaritraining" + new_date.to_datetime.strftime(" %d.%m.%Y %H:%M")
+              return "Shibaritraining" + new_date
             end
-          end
-
-          def set_taxons taxon_ids
-            taxons = taxon_ids.split(",").reverse().map { |t| ::Spree::Taxon.find t }
-            taxonomies = {}
-            taxons.each do |t|
-              taxonomies[t.taxonomy] = t.id if taxonomies.exclude? t.taxonomy          
-            end
-            return_taxon_ids = []
-            taxonomies.each { |k,v| return_taxon_ids << v }
-            return return_taxon_ids
           end
 
           def set_sku params, new_date
@@ -52,22 +43,19 @@ module Dojo
             return "k-#{taxon_ids}-#{new_date.to_datetime.strftime("%Y%m%d%H%M")}"
           end
           
-          def set_stock_items product
-            product.stock_items.first.adjust_count_on_hand Dojo::Application.config.default_count_on_hand
-            product.stock_items.first.update_attribute(:backorderable, false)
-            product.save
+          def set_stock_items
+            @product.stock_items.first.adjust_count_on_hand Dojo::Application.config.default_count_on_hand
+            @product.stock_items.first.update_attribute(:backorderable, false)
           end
 
-          def set_default_image product
-            image = File.open(Rails.root.join("app", "assets", "images", "placeholder_products.jpeg"))
-            product.images.create(attachment: image)
-            product.save
+          def set_default_image
+            @product.images.create(attachment: @image)
           end
 
           def create_bulk params
             count_date = params[:product][:date_and_time]
             for i in 1..params[:repeat_amount].to_i do
-              product = ::Spree::Product.create(
+              @product = ::Spree::Product.create(
                 name: set_default_name(params, count_date),
                 sku: set_sku(params, count_date),
                 description: params[:product][:description],
@@ -83,9 +71,9 @@ module Dojo
               else
                 count_date = params[:product][:date_and_time].to_datetime + i.months
               end
-              set_stock_items(product)
-              set_default_image(product)
-              product.save
+              set_stock_items
+              set_default_image
+              @product.save
             end
           end
 
