9323c39166d012df7ff9a1257387e04aad0e6dac
= <aj@anserjan.de>
2024-02-10 14:31:08 +0100

    decrease default count on hand

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dojo
       end
     end
 
-    config.default_count_on_hand = 15
+    config.default_count_on_hand = 8
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
