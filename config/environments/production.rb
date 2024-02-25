e59d0c7fbcf2d4f454112d0e0490073f50b81643
= <aj@anserjan.de>
2024-02-25 20:06:54 +0100

    add ionos smtp emailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dojo_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    authentication:  'plain',
+    enable_starttls: true,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
