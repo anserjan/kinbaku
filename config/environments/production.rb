2780f5afd7d7f7d1e2073a6b317cc9a59bcead9b
= <aj@anserjan.de>
2024-02-29 19:26:19 +0100

    add smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV.fetch("KINBAKU_SMTP_ADDRESS"),
     port: ENV.fetch("KINBAKU_SMTP_PORT"),
